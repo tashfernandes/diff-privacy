65c46d6f401cff5d4347ea8046b4ed8ae259be80
Natasha Fernandes <tashfernandes@gmail.com>
2016-11-13 12:25:46 +1100

    Added noise vector selection for logistic regression.

--- privacy.py
+++ privacy.py
@@ -29,16 +29,54 @@ def laplace_dist3(b):
     return np.random.laplace(scale=b)
     
 
+# Use Gamma distribution when using l2 norm as the metric for determining
+# distance between 2 datasets.
+
+# NOTES:
+# To add noise to the logistic regression function we need to use a noise vector
+# and add it to the calculated weight vector.
+# To do this we need to select the norm and direction for the vector.
+# The vector direction is selected from a gaussian distribution (each dimension
+# picked and then vector normalised). See http://mathworld.wolfram.com/HyperspherePointPicking.html
+# The norm is selected from the Gamma distribution.
+
+def generate_unit_vector(dimensions):
+    '''Selected a random (normalised) vector using Gaussian distribution.'''
+    v = np.random.normal(size=dimensions)
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+#
+# Inputs:
+# lda - the 'lambda' value calculated from regularisation
+# training_set_size - the number of training examples
+# epsilon - the value for the privacy budget
+#
+def generate_noise_vector(dimensions, lda, epsilon, training_set_size):
+    '''Generate a noise vector for logistic regression according to Gamma distribution.'''
+    v_dir = generate_unit_vector(dimensions)
+    scale = 2 / (training_set_size * epsilon * lda)
+    v_length = np.random.gamma(dimensions, scale=scale)
+    return v_dir * v_length
+
+
 #####################
 
+def test_unit_vector():
+    v = generate_unit_vector(10)
+    print "Should be on unit circle: %f" % sum(v * v)
 
 ### main
 
-x = []
-for i in range(10000):
-    x.append(laplace_dist3(1.0))
+test_unit_vector()
 
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
-plt.grid(True)
-plt.show()
+#x = []
+#for i in range(10000):
+#    x.append(laplace_dist3(1.0))
+#
+#n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+#plt.grid(True)
+#plt.show()
 
