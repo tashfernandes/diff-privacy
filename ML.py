ba32a77b57a755eb8373ffcce024d0cd1c47fddc
Natasha Fernandes <tashfernandes@gmail.com>
2016-11-18 16:43:45 +1100

    Change interface to support naive bayes

--- ML.py
+++ ML.py
@@ -10,40 +10,46 @@ class MLDataSet:
     def target(self):
         return self._target
 
-class MLData:
-    '''This class holds data for use in machine learning.'''
+class MLDataGenerator:
 
+    ## Class method for creating datasets for use in MLData
     # Subset holds the number of elements to select for the dataset
     # Split holds the fraction of elements in the full dataset to use in training
-    def __init__(self, data, subset=None, split=None):
-        # Split into training and test
-        self._training, self._test = self._split( data, subset, split)
+    # Data class has methods data() and target() to return the full dataset and target data    
 
-    def _split(self, data, subset, percent):
-        '''Split the data into training and test sets. Assumes the dataset is already randomised.'''
+    @staticmethod
+    def createTrainTestData(data, subset=None, split=None):
 
-        full_data = data.get_data()
-        full_target = data.get_target()
+        full_data = data.data()
+        full_target = data.target()
 
         if subset != None:
             full_data = full_data[0:subset]
             full_target = full_target[0:subset]
 
-        if percent == None:
+        if split == None:
             return MLDataSet(full_data, full_target), MLDataSet(full_data, full_target)
 
-        train_length = int(percent * len(full_data))
+        train_length = int(split * len(full_data))
         training_data = full_data[0:train_length]
         training_target = full_target[0:train_length]
         test_data = full_data[train_length:]
         test_target = full_target[train_length:]
         return MLDataSet(training_data, training_target), MLDataSet(test_data, test_target)
 
+class MLData:
+    '''This class holds data for use in machine learning.'''
+    # train_dataset and test_dataset have type which contains data and target
+    def __init__(self, train_dataset, test_dataset):
+        self._training = MLDataSet(train_dataset.data(), train_dataset.target())
+        self._test = MLDataSet(test_dataset.data(), test_dataset.target())
+
     def training(self):
         return self._training
 
     def test(self):
         return self._test
+
 
 class MLAlgorithm:
     '''Holds the algorithm used for machine learning.'''
