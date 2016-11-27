054e8cd169c45835da0debd93257567aa0f8cc8c
Natasha Fernandes <tashfernandes@gmail.com>
2016-11-27 23:34:31 +1100

    Syntax changes

--- run-forest.py
+++ run-forest.py
@@ -3,43 +3,45 @@
 from DPData import CsvData
 from DPData import ForestData
 from ML import MLDataGenerator
-from Algorithms import Regression
-from Algorithms import DPRegression
-from Privacy import Privacy
+from Algorithms import Regression, DPRegression
+from Privacy import DiffPrivacy
 
 import csv
 
-dataset = ForestData( CsvData('../forestdata/raw/tx_regression.csv') )
+for trial in range(10):
+    dataset = ForestData( CsvData('../forestdata/raw/tx_regression.csv') )
 
-results = []
+    results = []
 
-for subset in (1000, 10000, 50000, 100000, 200000, 500000, None):
-    training, test = MLDataGenerator.createTrainTestData(dataset, subset=subset, split=0.8)
+    for subset in (1000, 10000, 50000, 100000, 200000, 500000, None):
+        training, test = MLDataGenerator.createTrainTestData(dataset, subset=subset, split=0.8)
 
-    for C in (10.0,2.0,1.0,0.1,0.05,0.01,0.001):
-        regr = Regression( { 'lambda' : C } )
-        regr.train(training.data(), training.target())
-        report = regr.test(test.data(), test.target())
+        for lda in (10.0,2.0,1.0,0.1,0.05,0.01,0.001):
+            regr = Regression( { 'lambda' : lda } )
+            regr.train(training.data(), training.target())
+            report = regr.test(test.data(), test.target())
 
-        acc = report.accuracy()
+            acc = report.accuracy()
 
-        # Now let's add differential privacy to the model
-        training_data = training.data()
+            # Now let's add differential privacy to the model
+            training_data = training.data()
 
-        for epsilon in (1.0, 0.8, 0.5, 0.1, 0.08, 0.05, 0.01, 0.005, 0.001):
-            privacy = Privacy(lda=C, data_size=len(training_data), dim=training_data.shape[1], epsilon=epsilon)
-            private_regr = DPRegression( regr.model(), privacy )
-            dp_report = private_regr.test(test.data(), test.target())
+            for epsilon in (10.0, 8.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.8, 0.5, 0.1, 0.08, 0.05, 0.01, 0.005, 0.001):
+                privacy = DiffPrivacy(epsilon=epsilon)
+                regr_params = {'lambda' : lda, 'data_size' : len(training_data), 'dim' : training_data.shape[1]}
+                private_regr = DPRegression( regr.model(), regr_params, privacy )
+                dp_report = private_regr.test(test.data(), test.target())
 
-            dp_acc = dp_report.accuracy()
+                dp_acc = dp_report.accuracy()
 
-            results.append( { 'size' : len(training_data), 'epsilon': epsilon, 'lambda' : C, 'accuracy' : acc, 'dp_accuracy' : dp_acc } )
+                results.append( { 'size' : len(training_data), 'epsilon': epsilon, 'lambda' : lda, 'accuracy' : acc, 'dp_accuracy' : dp_acc } )
 
 
-if len(results) > 0:
-    output_file = open('results.csv', 'wb')
-    writer = csv.DictWriter(output_file, fieldnames = results[0].keys())
-    writer.writeheader()
-    for row in results:
-        writer.writerow(row)
-    output_file.close()
+    if len(results) > 0:
+        filename = 'results_' + str(trial) + '.csv'
+        output_file = open(filename, 'wb')
+        writer = csv.DictWriter(output_file, fieldnames = results[0].keys())
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
+        output_file.close()
--- run-newsgroups.py
+++ run-newsgroups.py
@@ -1,24 +1,52 @@
 #!/usr/local/bin/python
 
 from DPData import NewsgroupsData
+from DPData import VectorData
 from ML import MLData
-from Algorithms import NaiveBayes
-from Privacy import Privacy
+from Algorithms import Regression, DPRegression
+from Privacy import DiffPrivacy
 
 import csv
 
 categories = None
 
-dataset = NewsgroupsData(categories=categories)
+# For this data we run using differing sensitivities
+
+dataset = VectorData(NewsgroupsData(categories=categories))
 
 mldata = MLData(dataset.training(), dataset.test())
 training = mldata.training()
 test = mldata.test()
 
-model = NaiveBayes({'alpha' : 0.01})
-model.train(training.data(), training.target())
-report = model.test(test.data(), test.target())
+lda = 10.0
+params = {'lambda': lda}
+regr = Regression(params)
+regr.train(training.data(), training.target())
+report = regr.test(test.data(), test.target())
 
 acc = report.accuracy()
 print "Got accuracy: ", acc
+
+results = []
+training_data = training.data()
+
+# Now let's add differential privacy to the model
+for epsilon in (10.0, 8.0, 5.0, 3.0, 1.0, 0.8, 0.5, 0.3, 0.1, 0.08, 0.05, 0.03, 0.01, 0.008, 0.005, 0.003, 0.001, 0.0008, 0.0005, 0.0001):
+    privacy_params = {'lambda' : lda, 'data_size' : training_data.shape[0], 'dim' : training_data.shape[1]}
+    privacy = DiffPrivacy(epsilon=epsilon, params=privacy_params)
+    private_regr = DPRegression( regr.model(), regr_params )
+    dp_report = private_regr.test(test.data(), test.target())
+
+    dp_acc = dp_report.accuracy()
+
+    results.append( { 'size' : training_data.shape[0], 'epsilon': epsilon, 'lambda' : lda, 'accuracy' : acc, 'dp_accuracy' : dp_acc } )
+
+if len(results) > 0:
+    filename = 'ng_results.csv'
+    output_file = open(filename, 'wb')
+    writer = csv.DictWriter(output_file, fieldnames = results[0].keys())
+    writer.writeheader()
+    for row in results:
+        writer.writerow(row)
+    output_file.close()
 
