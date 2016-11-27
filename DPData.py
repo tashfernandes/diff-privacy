b6fd424dd92d3ef0fefee5a480cb87d905a6735e
Natasha Fernandes <tashfernandes@gmail.com>
2016-11-27 23:34:06 +1100

    Add vectorized data

--- DPData.py
+++ DPData.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import csv
 
@@ -38,8 +39,12 @@ class ForestData:
             data.append(vals_converted)
             target.append(y)
 
-        self._data = np.array(data)
-        self._target = np.array(target)
+        # randomise the data
+        rand_array = np.random.permutation(len(data))
+        d = np.array(data)
+        self._data = d[rand_array]
+        t = np.array(target)
+        self._target = t[rand_array]
 
     def data(self):
         return self._data
@@ -62,8 +67,8 @@ class NewsgroupDataSet:
 class NewsgroupsData:
 
     def __init__(self, categories):
-        self._training = NewsgroupDataSet( fetch_20newsgroups(subset='train', categories=categories) )
-        self._test = NewsgroupDataSet(fetch_20newsgroups(subset='test', categories=categories))
+        self._training = NewsgroupDataSet( fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'), categories=categories) )
+        self._test = NewsgroupDataSet(fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'), categories=categories))
 
     def training(self):
         return self._training
@@ -71,3 +76,34 @@ class NewsgroupsData:
     def test(self):
         return self._test
 
+class VectorDataSet:
+
+    def __init__(self, data, target):
+        self._data = data
+        self._target = target
+
+    def data(self):
+        return self._data
+
+    def target(self):
+        return self._target
+
+class VectorData:
+
+    def __init__(self, datamodel):
+        self._vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+        training = datamodel.training()
+        train_data = self._vectorizer.fit_transform(training.data())
+        train_target = training.target()
+        self._training = VectorDataSet(train_data, train_target)
+
+        test = datamodel.test()
+        test_data = self._vectorizer.transform(test.data())
+        test_target = test.target()
+        self._test = VectorDataSet(test_data, test_target)
+
+    def training(self):
+        return self._training
+
+    def test(self):
+        return self._test
